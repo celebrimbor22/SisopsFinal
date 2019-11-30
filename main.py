3b32e86cfd455d1d87872fa48cfe23bfcef2b9ed
codyvi <davidcantumartinez@gmail.com>
2019-11-30 15:48:18 -0600

    Doing this

--- main.py
+++ main.py
@@ -2,33 +2,28 @@ import sys
 from instructionReader import parse_instructions
 import instructions
 
-def main():
-    print('******************************')
-    print('******************************')
-    print('******************************')
-    print('* Manejador de Memoria V 1.0 *')
-    print('******************************')
-    print('******************************')
-    print('******************************')
+print('******************************')
+print('******************************')
+print('******************************')
+print('* Manejador de Memoria V 1.0 *')
+print('******************************')
+print('******************************')
+print('******************************')
+
+var = input('Dame la estrategia (fifo o lru): ')
+while( var != 'fifo' and var != 'lru' ):
+    print('Solo existe fifo o lru en este manejador')
     var = input('Dame la estrategia (fifo o lru): ')
-    while( var != 'fifo' and var != 'lru' ):
-        print('Solo existe fifo o lru en este manejador')
-        var = input('Dame la estrategia (fifo o lru): ')
-
-    print('La estrategia a urilizar es', var)
-
-    # Sets strategy on instructions
-    instructions.STRATEGY = True if var == 'fifo' else False
-
-    parsed_instructions = parse_instructions()
-    for instruction in parsed_instructions:
-        print("\n", ' '.join(str(x) for x in instruction), sep="")
-    # Add calls to each instruction
-        if instruction[0] == 'P':
-            instructions.P(instruction[1], instruction[2])
-        elif instruction[0] == 'A':
-            instructions.A(instruction[1], instruction[2], instruction[3])
-        elif instruction[0] == 'E':
-            instructions.E()
-
-main()
+print('La estrategia a urilizar es', var)
+# Sets strategy on instructions
+instructions.STRATEGY = True if var == 'fifo' else False
+parsed_instructions = parse_instructions()
+for instruction in parsed_instructions:
+    print("\n", ' '.join(str(x) for x in instruction), sep="")
+# Add calls to each instruction
+    if instruction[0] == 'P':
+        instructions.P(instruction[1], instruction[2])
+    elif instruction[0] == 'A':
+        instructions.A(instruction[1], instruction[2], instruction[3])
+    elif instruction[0] == 'E':
+        instructions.E()
