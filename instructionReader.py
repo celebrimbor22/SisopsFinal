1705b991012ac46e6f923907968ee6ed8968226e
andres miguel <49008359+celebrimbor22@users.noreply.github.com>
2019-11-30 14:16:02 -0600

    Update instructionReader.py

--- instructionReader.py
+++ instructionReader.py
@@ -1,83 +1,85 @@
 import os
+import sys
 
 def parse_instructions():
-    instructions = []
+    #lista de instrucciones 
+    instr_list = []
     # archivo de instrucciones
-    user_input = input('Ingresa el archivo de instrucciones: ')
-    file_path = user_input.rstrip('\r')
+    archivo = input('Ingresa el archivo de instrucciones: ')
+    #direccion de archivo de instrucciones 
+    arch_path = archivo.rstrip('\r')
 
     # verificar que el archivo existe
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(arch_path):
         print('El archivo especificado no se encontró en el directorio.')
         print('El programa finaliza su ejecución tras no encontrar el archivo de entrada.')
-
-        exit()
+        sys.exit()
     
-    with open(file_path.rstrip('\r')) as file:
+    with open(arch_path.rstrip('\r')) as file:
         # lee todas las líneas del archivo y las almacena en un arreglo con nombre lines
-        lines = file.read().splitlines()
+        instruccion = file.read().splitlines()
         
-        for i, line in enumerate(lines):
+        for i, instruccion in enumerate(instruccion):
             # separa las líneas de instrucción en sus parámetros
-            words = line.split(' ')
+            accion = instruccion.split(' ')
             # procesamiento de la instrucción dependiendo del comando proporcionado por la primera letra
-            if words[0] == 'A':
+            if accion[0] == 'A':
                 # revisa que la instrucción cuente con el número de parámetros requeridos
-                if(len(words) < 4):
+                if(len(accion) < 4):
                     print('El comando de la línea ', i+1, 'requiere 4 parámetros.')
                     print('El programa finaliza su ejecución por encontrar un comando inválido.')
                     exit()
-                instruction = [words[0]]
+                instr_list = [accion[0]]
                 # convierte a entero los parámetros del comando
                 try:
-                    instruction.append(int(words[1]))
-                    instruction.append(int(words[2]))
-                    instruction.append(int(words[3]))
+                    instr_list.append(int(accion[1]))
+                    instr_list.append(int(accion[2]))
+                    instr_list.append(int(accion[3]))
                 except ValueError:
                     print('Hay uno o más parámetros incorrectos en la instrucción que está en la línea', i+1)
                     print('El programa finaliza su ejecución por encontrar un parámetro inválido.')
-                    exit()
-            elif words[0] == 'P':
+                    sys.exit()
+            elif accion[0] == 'L':
                 # revisa que la instrucción cuente con el número de parámetros requeridos
-                if(len(words) < 3):
-                    print('El comando de la línea ', i+1, 'requiere 3 parámetros.')
-                    print('El programa finaliza su ejecución por encontrar un comando inválido.')
-                    exit()
-                instruction = [words[0]]
-                # convierte a entero los parámetros del comando
-                try:
-                    instruction.append(int(words[1]))
-                    instruction.append(int(words[2]))
-                except ValueError:
-                    print('Hay uno o más parámetros incorrectos en la instrucción que está en la línea', i+1)
-                    print('El programa finaliza su ejecución por encontrar un parámetro inválido.')
-                    exit()
-            elif words[0] == 'L':
-                # revisa que la instrucción cuente con el número de parámetros requeridos
-                if(len(words) < 2):
+                if(len(accion) < 2):
                     print('El comando de la línea ', i+1, 'requiere 2 parámetros.')
                     print('El programa finaliza su ejecución por encontrar un comando inválido.')
-                    exit()
-                instruction = [words[0]]
+                    sys.exit()
+                instr_list = [accion[0]]
                 # convierte a entero los parámetros del comando
                 try:
-                    instruction.append(int(words[1]))
+                    instr_list.append(int(accion[1]))
                 except ValueError:
                     print('Hay uno o más parámetros incorrectos en la instrucción que está en la línea', i+1)
                     print('El programa finaliza su ejecución por encontrar un parámetro inválido.')
-                    exit()
-            elif words[0] == 'C':
-                instruction = [words[0]]
+                    sys.exit()
+            elif accion[0] == 'C':
+                instr_list = [accion[0]]
                 # vuelve a juntar el arreglo de parámetros para imprimir el comentario como salida
-                instruction.append(' '.join(words[1::]))
-            elif words[0]== 'E':
-                instruction = [words[0]]
-            elif words[0] == 'F':
-                instruction = [words[0]]
+                instruction.append(' '.join(accion[1::]))
+			elif accion[0] == 'F':
+                instr_list = [accion[0]]
+			elif accion[0] == 'P':
+                # revisa que la instrucción cuente con el número de parámetros requeridos
+                if(len(accion) < 3):
+                    print('El comando de la línea ', i+1, 'requiere 3 parámetros.')
+                    print('El programa finaliza su ejecución por encontrar un comando inválido.')
+                    sys.exit()
+                instr_list = [accion[0]]
+                # convierte a entero los parámetros del comando
+                try:
+                    instr_list.append(int(accion[1]))
+                    instr_list.append(int(accion[2]))
+                except ValueError:
+                    print('Hay uno o más parámetros incorrectos en la instrucción que está en la línea', i+1)
+                    print('El programa finaliza su ejecución por encontrar un parámetro inválido.')
+                    sys.exit()
+            elif accion[0]== 'E':
+                instr_list = [accion[0]]
             else: 
                 # instrucción inválida
                 print("Instrucción desconocida en la línea ", i+1)
                 print('El programa finaliza su ejecución por encontrar un comando desconocido.')
-                exit()
-            instructions.append(instruction)
-        return instructions
+                sys.exit()
+            instr_list.append(instr_list)
+        return instr_list
